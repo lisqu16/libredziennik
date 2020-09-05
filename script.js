09af1fe8e1e7dbb30e7901a8b6ceee96908e0897
ProgramistaZpolski <piotrekbadelek@gmail.com>
2020-09-05 09:28:16 +0200

    strona logowania

---
+++ script.js
@@ -0,0 +1,23 @@
+function welcome(){
+    document.getElementById('logprog').style.display = "block";
+    document.getElementById('firstshow').style.display = "none";
+    document.getElementById('butony').style.display = "none";
+    document.getElementById('nejm').innerText = document.getElementById('logoin').value;
+}
+/* Dynamiczna tapeta czy coś w tym stylu */
+var d = new Date();
+var n = d.getHours();
+if (n > 4){
+    console.log("wiecej niz 4")
+    if (n < 9){
+        console.log("mniej niz 9")
+        // uruchamia się tylko jeśli jest 4:00 do 8:59
+        document.body.style.backgroundImage = "url('img/morning.webp')";
+    }
+    else{
+        document.body.style.backgroundImage = "url('img/day.webp')";
+    }
+}
+else{
+    document.body.style.backgroundImage = "url('img/day.webp')";
+}
